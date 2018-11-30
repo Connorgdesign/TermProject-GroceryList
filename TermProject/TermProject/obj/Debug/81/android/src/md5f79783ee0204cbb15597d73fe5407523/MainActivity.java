591a20e920822676906e4baa3931a5f57ebbf3ff
Connorgdesign <connor.gdesign@gmail.com>
2018-11-29 23:09:57 -0800

    Main Layout
    The main layout allows the user to enter a new grocery list item an estimated price and the type of item they are entering.

---
+++ TermProject/TermProject/obj/Debug/81/android/src/md5f79783ee0204cbb15597d73fe5407523/MainActivity.java
@@ -0,0 +1,47 @@
+package md5f79783ee0204cbb15597d73fe5407523;
+
+
+public class MainActivity
+	extends android.support.v7.app.AppCompatActivity
+	implements
+		mono.android.IGCUserPeer
+{
+/** @hide */
+	public static final String __md_methods;
+	static {
+		__md_methods = 
+			"n_onCreate:(Landroid/os/Bundle;)V:GetOnCreate_Landroid_os_Bundle_Handler\n" +
+			"";
+		mono.android.Runtime.register ("TermProject.MainActivity, TermProject", MainActivity.class, __md_methods);
+	}
+
+
+	public MainActivity ()
+	{
+		super ();
+		if (getClass () == MainActivity.class)
+			mono.android.TypeManager.Activate ("TermProject.MainActivity, TermProject", "", this, new java.lang.Object[] {  });
+	}
+
+
+	public void onCreate (android.os.Bundle p0)
+	{
+		n_onCreate (p0);
+	}
+
+	private native void n_onCreate (android.os.Bundle p0);
+
+	private java.util.ArrayList refList;
+	public void monodroidAddReference (java.lang.Object obj)
+	{
+		if (refList == null)
+			refList = new java.util.ArrayList ();
+		refList.add (obj);
+	}
+
+	public void monodroidClearReferences ()
+	{
+		if (refList != null)
+			refList.clear ();
+	}
+}
